eb040ef4d983d9a4b1546f5c1f9f56d568e3782c
huangxue2019 <1659530389@qq.com>
2019-05-07 17:28:32 +0800

    提交到本次仓库

---
+++ student_Pro/src/com/student/model/studentModel.java
@@ -0,0 +1,21 @@
+package com.student.model;
+
+public class studentModel {
+
+	private int id;	
+	private String username;
+	private String password;
+	private int sex;
+	private String alaso;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
